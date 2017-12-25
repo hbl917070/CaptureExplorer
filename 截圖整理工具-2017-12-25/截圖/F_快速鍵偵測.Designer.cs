a116424f95298a5ecb6c7676ec0adf0f8b49a37b
hbl917070 <hbl917070@hotmail.com.tw>
2017-12-26 02:50:34 +0800

    1
    1

---
+++ 截圖整理工具-2017-12-25/截圖/F_快速鍵偵測.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace 截圖 {
+    partial class F_快速鍵偵測 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.keyboardHook1 = new WindowsHookLib.KeyboardHook();
+            this.SuspendLayout();
+            // 
+            // F_快速鍵偵測
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(0, 0);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "F_快速鍵偵測";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "F_快速鍵偵測";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WindowsHookLib.KeyboardHook keyboardHook1;
+    }
+}
